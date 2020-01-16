a1faa163ccd54cc37a5d1e30ce6632163538474c
josephkingstone <joseph.kingstone@icloud.com>
2020-01-16 10:57:23 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,170 @@
+#/bin/bash
+
+#Download Internal-Monologue
+
+repository="https://github.com/eladshamir/Internal-Monologue.git"
+
+localFolder="/opt/csharptooling/internalmonologue/"
+
+git clone "$repository" "$localFolder"
+
+
+#Download SharpWeb
+
+repository="https://github.com/djhohnstein/SharpWeb.git"
+
+localFolder="/opt/csharptooling/sharpweb/"
+
+git clone "$repository" "$localFolder"
+
+
+#Download SharpHound
+
+repository="https://github.com/BloodHoundAD/SharpHound.git"
+
+localFolder="/opt/csharptooling/sharphound/"
+
+git clone "$repository" "$localFolder"
+
+
+#Download InveighZero
+
+repository="https://github.com/Kevin-Robertson/InveighZero.git"
+
+localFolder="/opt/csharptooling/inveighzero/"
+
+git clone "$repository" "$localFolder"
+
+#Download SharpExec
+
+repository="https://github.com/anthemtotheego/SharpExec.git"
+
+localFolder="/opt/csharptooling/sharpexec/"
+
+git clone "$repository" "$localFolder"
+
+#Download SharPersist
+
+repository="https://github.com/fireeye/SharPersist.git"
+
+localFolder="/opt/csharptooling/sharpersist/"
+
+git clone "$repository" "$localFolder"
+
+#Download SharpCom
+
+repository="https://github.com/rvrsh3ll/SharpCOM.git"
+
+localFolder="/opt/csharptooling/sharpcom/"
+
+git clone "$repository" "$localFolder"
+
+#Download SharpPrinter
+
+repository="https://github.com/rvrsh3ll/SharpPrinter.git"
+
+localFolder="/opt/csharptooling/sharpprinter/"
+
+git clone "$repository" "$localFolder"
+
+#Download SharpFruit
+
+repository="https://github.com/rvrsh3ll/SharpFruit.git"
+
+localFolder="/opt/csharptooling/sharpfruit/"
+
+git clone "$repository" "$localFolder"
+
+#Download SharpExcel4-DCOM
+
+repository="https://github.com/rvrsh3ll/SharpExcel4-DCOM.git"
+
+localFolder="/opt/csharptooling/sharpexcel4dcom/"
+
+git clone "$repository" "$localFolder"
+
+#Download ADFS Dump
+
+repository="https://github.com/fireeye/ADFSDump.git"
+
+localFolder="/opt/csharptooling/adfsdump/"
+
+git clone "$repository" "$localFolder"
+
+#Download OffensiveCSharp
+
+repository="https://github.com/matterpreter/OffensiveCSharp.git"
+
+localFolder="/opt/csharptooling/offensivecsharp/"
+
+git clone "$repository" "$localFolder"
+
+#Download SharpView
+
+repository="https://github.com/tevora-threat/SharpView.git"
+
+localFolder="/opt/csharptooling/sharpview/"
+
+git clone "$repository" "$localFolder"
+
+#Download SharpDomainSpray
+
+repository="https://github.com/HunnicCyber/SharpDomainSpray.git"
+
+localFolder="/opt/csharptooling/sharpdomainspray/"
+
+git clone "$repository" "$localFolder"
+
+
+#Download SharpSniper
+
+repository="https://github.com/HunnicCyber/SharpSniper.git"
+
+localFolder="/opt/csharptooling/sharpsniper/"
+
+git clone "$repository" "$localFolder"
+
+
+#Download SeatBelt
+
+repository="https://github.com/GhostPack/Seatbelt.git"
+
+localFolder="/opt/csharptooling/seatbelt/"
+
+git clone "$repository" "$localFolder"
+
+
+#Download Rubeus
+
+repository="https://github.com/GhostPack/Rubeus.git"
+
+localFolder="/opt/csharptooling/rubeus/"
+
+git clone "$repository" "$localFolder"
+
+
+#Download SharpUp
+
+repository="https://github.com/GhostPack/SharpUp.git"
+
+localFolder="/opt/csharptooling/sharpup/"
+
+git clone "$repository" "$localFolder"
+
+
+#Download SafetyKatz
+
+repository="https://github.com/GhostPack/SafetyKatz.git"
+
+localFolder="/opt/csharptooling/safetykatz/"
+
+git clone "$repository" "$localFolder"
+
+
+#Download SharpWMI
+
+repository="https://github.com/GhostPack/SharpWMI.git"
+
+localFolder="/opt/csharptooling/sharpwmi/"
+
+git clone "$repository" "$localFolder"
